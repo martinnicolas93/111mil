ac958934adcba37603a4ccfc8dadc2c5f4f2d782
Martin Nicolas <32282077+martinnicolas93@users.noreply.github.com>
2017-11-16 16:36:13 -0300

    Delete ejercicio5.sql

--- baseDatos/ejercicio5.sql
+++
@@ -1,9 +0,0 @@
--- Determinar la sumatoria de los precios de todas las propiedades que están en venta y aún no han sido vendidas.
-
-SELECT 
-	sum(price)
-FROM
-	property
-WHERE 
-	operation = 'Venta'
-	AND selling_timestamp IS NULL;
